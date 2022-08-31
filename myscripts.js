d6c72ceae8a8b83cb99552fd6cf20f346ad0eada
evolF35 <maccyair@gmail.com>
2022-08-30 17:32:53 -0700

    added some buttons that change colors

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-let gridsize = 10;
+let gridsize = 16;
 const container = document.querySelector('#Box');
 
 let scaler = 0;
@@ -10,13 +10,22 @@ function opacity(gridsize){
 }
 scaler = opacity(gridsize);
 
+
+let random = false;
+let progressive = false;
+let inputOn = false;
+
+
+
+
+
 function Row(gridsize) {
     for(let i = 0; i < gridsize; i++){
     const zapatos = document.createElement("div");
     zapatos.setAttribute('id','Row');
     zapatos.style.display = "flex";
-    zapatos.style.width = '960px';
-    zapatos.style.height = '960px';
+    zapatos.style.width = '700px';
+    zapatos.style.height = '700px';
     block(zapatos);
     container.appendChild(zapatos);
 }
@@ -25,10 +34,22 @@ function Row(gridsize) {
 function block(zapatos) { 
     for(let j = 0; j < gridsize; j++){
         const divTemplate = document.createElement("div");
+        divTemplate.setAttribute('class','unit');
         divTemplate.style.width = '100%';
         divTemplate.style.height = '100%';
         divTemplate.style.border = `${scaler}px solid black`;
         divTemplate.style.background = 'transparent';
+
+        divTemplate.addEventListener("mouseover", function handleMouseOver(){
+            divTemplate.style.background = 'black';
+            if(random === true){
+                divTemplate.style.background = `#${randomcolor()}`;
+            }
+            if(inputOn == true){
+                divTemplate.style.background = `${inputColor}`;
+            }
+        });
+
 
         zapatos.appendChild(divTemplate);
     }
@@ -40,7 +61,7 @@ const chooseButton = document.querySelector('#chooseSize');
 
 chooseButton.addEventListener('click',() =>{
     const boxes = document.querySelectorAll('#Row');
-1
+
     boxes.forEach(box => {
       box.remove();
     });
@@ -55,4 +76,45 @@ chooseButton.addEventListener('click',() =>{
 
 
 
+function randomcolor(){
 
+    let color = "";
+    for(let i = 0; i < 6; i++){
+        let r = `${Math.floor(Math.random() * 10)}`;
+        color += r;
+        }
+    return(color);
+}
+
+const randombutton = document.querySelector('#randomColor');
+
+randombutton.addEventListener('click',() =>{
+    if(random === false){
+        random = true;
+        document.getElementById('randomColor').innerText = "Random Color Mode: On";
+    }
+    else{
+        random = false;
+        document.getElementById('randomColor').innerText = "Random Color Mode: Off";
+    }
+
+    
+});
+
+let inputColor = "#000000";
+
+const pickColorButton = document.querySelector('#pickColor');
+
+pickColorButton.addEventListener('click',() =>{
+    if(inputOn == false){
+    inputOn = true;
+    inputColor = prompt("Input a hex color");
+    document.getElementById('pickColor').innerText = `Input Color Mode: ${inputColor}`;
+}
+    else{
+        inputOn = false;
+        document.getElementById('pickColor').innerText = `Input Color Mode: Off`;
+    }
+    
+
+});
